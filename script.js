295bd9524671aa2628af6af7a0557c737510a2e2
aldzin <152745473+aldzin@users.noreply.github.com>
2023-12-03 06:44:17 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 yesBtn.addEventListener("click", () => {
 question.innerHTML = "Aaaaa, I like you too";
-gif.src ="https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; 
+gif.src =
+"https://raw.githubusercontent.com/DzarelDeveloper/Img/main/gif.webp"; 
 });
 noBtn.addEventListener("mouseover", () => 
 const noBtnRect = noBtn.getBoundingClientRect();
